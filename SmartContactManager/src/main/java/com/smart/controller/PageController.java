d1468092e30d6d3cc36cdae23d2c856df7cfd112
akumar647_comcast <akumar647@comcast.com>
2025-06-27 21:13:09 +0530

    code changes for smart contact manager

---
+++ SmartContactManager/src/main/java/com/smart/controller/PageController.java
@@ -0,0 +1,25 @@
+//package com.smart.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class PageController {
+//
+////    @RequestMapping("/home")
+////    public String home(){
+////        System.out.println("Home page controller");
+////        return "home";
+////    }
+//
+//    //sending dynamic data
+//    @RequestMapping("/home")
+//    public String home(Model model){
+//       System.out.println("Home page controller");
+//       model.addAttribute("name","Ankit Kumar");
+//       model.addAttribute("Designation","SoftwareEngineer");
+//       return "home";
+//    }
+//
+//}
---
+++ SmartContactManager/src/main/java/com/smart/dao/UserRepository.java
@@ -0,0 +1,8 @@
+//package com.smart.dao;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository< User, Integer> {
+//
+//
+//}
